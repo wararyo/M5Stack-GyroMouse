7ecfa9ac9f84d21e6db64ac4d0186ae28b177530
wararyo <wararyo@gmail.com>
2019-05-17 17:05:00 +0900

    動いたがまた動かなくなった…

--- src/BLEMouse.h
+++ src/BLEMouse.h
@@ -5,15 +5,18 @@
 #include <BLE2902.h>
 #include <BLEHIDDevice.h>
 #include <Arduino.h>
+#include <esp_log.h>
 
 #define DEVICE_NAME "M5StackMouse"
 #define MANUFACTURER "wararyo"
+
+static char LOG_TAG[] = "BLEMouse";
  
 // HID input report data.
 typedef struct {
 	uint8_t buttons;
-    uint8_t x;
-    uint8_t y;
+    int8_t x;
+    int8_t y;
 } mouse_report_t;
  
 // HID report desc (mouse).
@@ -50,28 +53,57 @@ class BLEMouse;
 
 extern BLEMouse Mouse;
 
-class BLEMouse {
-  class ServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-        Mouse.isConnected = true;
-        Serial.println("ServerCallbacks::onConnect()");
-    };
-  
-    void onDisconnect(BLEServer* pServer) {
-        Mouse.isConnected = false;
-        Serial.println("ServerCallbacks::onDisconnect()");
-    }
-  };
+class MySecurity : public BLESecurityCallbacks {
+    uint32_t passKey = 0;
 
-	bool isConnected = false;
+    uint32_t onPassKeyRequest(){
+        ESP_LOGE(LOG_TAG, "The passkey request %d", passKey);
+
+        vTaskDelay(25000);
+        return passKey;
+    }
+    void onPassKeyNotify(uint32_t pass_key){
+        ESP_LOGE(LOG_TAG, "The passkey Notify number:%d", pass_key);
+        passKey = pass_key;
+    }
+    bool onSecurityRequest(){
+        return true;
+    }
+    void onAuthenticationComplete(esp_ble_auth_cmpl_t auth_cmpl){
+        if(auth_cmpl.success){
+            ESP_LOGI(LOG_TAG, "remote BD_ADDR:");
+            esp_log_buffer_hex(LOG_TAG, auth_cmpl.bd_addr, sizeof(auth_cmpl.bd_addr));
+            ESP_LOGI(LOG_TAG, "address type = %d", auth_cmpl.addr_type);
+        }
+        ESP_LOGI(LOG_TAG, "pair status = %s", auth_cmpl.success ? "success" : "fail");
+    }
+    bool onConfirmPIN(uint32_t pin) {return true;};
+};
+
+class BLEMouse {
+    class ServerCallbacks: public BLEServerCallbacks {
+        void onConnect(BLEServer* pServer) {
+            Mouse.isConnected = true;
+            Serial.println("ServerCallbacks::onConnect()");
+        };
+
+        void onDisconnect(BLEServer* pServer) {
+            Mouse.isConnected = false;
+            Serial.println("ServerCallbacks::onDisconnect()");
+        }
+    };
+
 	BLEHIDDevice* hid;
 	BLEServer* pServer;
     BLECharacteristic* input;
+    BLECharacteristic* output;
 
 public:
+	bool isConnected = false;
 	void begin() {
 		BLEDevice::init(DEVICE_NAME);
-        BLEDevice::setEncryptionLevel((esp_ble_sec_act_t)ESP_LE_AUTH_REQ_SC_BOND);
+        BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT_NO_MITM);
+        BLEDevice::setSecurityCallbacks(new MySecurity());
 
 		pServer = BLEDevice::createServer();
 		pServer->setCallbacks(new ServerCallbacks());
@@ -80,15 +112,16 @@ public:
 		input = hid->inputReport(1);
         input->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
         input->addDescriptor(new BLE2902());
+        output = hid->outputReport(1);
 
 		hid->manufacturer()->setValue(MANUFACTURER);
 		hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
-		hid->hidInfo(0x00,0x01);  // country == 0, flags == 1 ( providing wake-up signal to a HID host)
+		hid->hidInfo(0x00,0x00);  // country == 0, flags == 1 ( providing wake-up signal to a HID host)
 		hid->reportMap((uint8_t*)reportMap, sizeof(reportMap));
 		hid->startServices();
         
 		BLEAdvertising *pAdvertising = pServer->getAdvertising();
-		pAdvertising->setAppearance(HID_KEYBOARD);
+		pAdvertising->setAppearance(HID_MOUSE);
 		pAdvertising->addServiceUUID(hid->hidService()->getUUID());
 
         BLESecurity *pSecurity = new BLESecurity();
@@ -100,11 +133,13 @@ public:
 		Serial.println("Start Advertising.");
 	}
  
-	void sendReport(uint8_t buttons, uint8_t x, uint8_t y) {
+	void sendReport(uint8_t buttons, int8_t x, int8_t y) {
 		if (isConnected) {
             mouse_report_t report = {buttons,x,y};
+
 			input->setValue((uint8_t*)&report, sizeof(mouse_report_t));
 			input->notify();
+		    Serial.print("*");
 		}
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <M5Stack.h>
 #include <M5StackUpdater.h>
+#include "utility/MPU9250.h"
  
 #include "BLEMouse.h"
+
+MPU9250 IMU;
  
 void setup() {
   M5.begin();
@@ -15,9 +18,37 @@ void setup() {
 
   Serial.begin(9600);
   Mouse.begin();
+  // if (0x71 != IMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250)) {
+  //   M5.Lcd.println("MPU9250 not connected...");
+  //   delay(2000);
+  // }
+  // IMU.initMPU9250();
+  // if ( 0x48 != IMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963)) {
+  //     M5.Lcd.println("AK8963 not connected...");
+  //     delay(2000);
+  //   }
+  // IMU.initAK8963(IMU.magCalibration);
 }
  
 void loop() {
+  //delay(16);
+  if(!Mouse.isConnected) return;
   M5.update();
-	if(M5.BtnA.isPressed()) Mouse.sendReport(0,1,1);
+	if(M5.BtnA.isPressed()) {
+    M5.Lcd.print("Left");
+    Mouse.sendReport(1,1,1);//Left Click, 1, 1
+  }
+  else if(M5.BtnA.wasReleased()) {
+    M5.Lcd.print("Released");
+    Mouse.sendReport(0,0,0);
+  }
+  // if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {
+  //   IMU.readGyroData(IMU.gyroCount);
+  //   IMU.getGres();
+  //   IMU.gx = (float)IMU.gyroCount[0]*IMU.gRes;
+  //   IMU.gy = (float)IMU.gyroCount[1]*IMU.gRes;
+  //   IMU.gz = (float)IMU.gyroCount[2]*IMU.gRes;
+  //   Mouse.sendReport(0,5+(int8_t)(IMU.gy),5+(int8_t)(IMU.gx));
+  //   //Serial.printf("%d %d %d \n",(int8_t)IMU.gx,(int8_t)IMU.gy,(int8_t)IMU.gz);
+  // }
 }
